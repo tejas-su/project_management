b9069de8f4b3b1f81f15d6fb1c3cae4757bebbac
tejas.s.u_ <90765561+tejas-su@users.noreply.github.com>
2024-02-23 23:04:37 +0530

    minor changes

--- lib/presentation/screens/signin_screen.dart
+++ lib/presentation/screens/signin_screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -19,7 +18,6 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  final storage = const FlutterSecureStorage();
   @override
   Widget build(BuildContext context) {
     //user controller
