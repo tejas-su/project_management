6f7f9e5ae28ec1aeb769b315a0a337421d07cd32
tejas.s.u_ <90765561+tejas-su@users.noreply.github.com>
2024-03-08 00:14:03 +0530

    create operations implemented

--- lib/presentation/screens/add_new_screen.dart
+++ lib/presentation/screens/add_new_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -14,6 +16,36 @@ class AddNewScreen extends StatefulWidget {
 class _AddNewScreenState extends State<AddNewScreen> {
   @override
   Widget build(BuildContext context) {
+    //project name controller
+    TextEditingController projectName = TextEditingController();
+    //project description controller
+    TextEditingController projectDesc = TextEditingController();
+    //user name controller
+    TextEditingController userName = TextEditingController();
+    TextEditingController name = TextEditingController();
+    //user email controller
+    TextEditingController userEmail = TextEditingController();
+    //user designation controller
+    TextEditingController userDesig = TextEditingController();
+    //bugs name controller
+    TextEditingController bugName = TextEditingController();
+    //bug status controller
+    TextEditingController bugStatus = TextEditingController();
+    //bugs description controller
+    TextEditingController bugsDesc = TextEditingController();
+    //function to dispose the text editing controller
+
+    void dispose() {
+      projectName.clear();
+      projectDesc.clear();
+      userName.clear();
+      userEmail.clear();
+      userDesig.clear();
+      name.clear();
+      bugName.clear();
+      bugsDesc.clear();
+    }
+
     //fetch the date
     DateTime now = DateTime.now();
     //formatted date in the format dd-mm-yyyy
@@ -24,22 +56,17 @@ class _AddNewScreenState extends State<AddNewScreen> {
     //width of the screen
     double width = MediaQuery.of(context).size.width;
     //Text Controllers for varios fields
-    //project name controller
-    TextEditingController projectName = TextEditingController();
-    //project description controller
-    TextEditingController projectDesc = TextEditingController();
-    //user name controller
-    TextEditingController userName = TextEditingController();
-    //user email controller
-    TextEditingController puserEmail = TextEditingController();
-    //user designation controller
-    TextEditingController userDesig = TextEditingController();
+
     //function to show error message
-    void showErrorDialog(BuildContext context, String message) {
+    void showErrorDialog(
+      BuildContext context,
+      String title,
+      String message,
+    ) {
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: const Text("Oops, something went wrong"),
+          title: Text(title),
           content: Text(message),
           actions: [
             TextButton(
@@ -56,15 +83,79 @@ class _AddNewScreenState extends State<AddNewScreen> {
     //fetch the project name then add other details as the project name is the primary key
     void saveProject() async {
       try {
-        var response = await widget.supabase.from('projects').insert({
-          'project_name': projectName.text,
-          'date_created': formattedDate,
-          'team_name': groupName,
-          'project_description': projectDesc.text
-        });
-        print(response);
+        if (projectName.text.isEmpty ||
+            projectDesc.text.isEmpty ||
+            userDesig.text.isEmpty ||
+            userName.text.isEmpty ||
+            userEmail.text.isEmpty ||
+            bugName.text.isEmpty ||
+            bugStatus.text.isEmpty) {
+          showErrorDialog(
+            context,
+            'Oops, something went wrong',
+            'Please fill in all the fields',
+          );
+        } else {
+          //show loading screen
+          showDialog(
+            context: context,
+            builder: (context) => Container(
+              color: whiteBG,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Lottie.asset('lottie/ani1.json', height: 500),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    'Saving your data, this might take some time',
+                    style: GoogleFonts.dmSerifDisplay(
+                        fontWeight: FontWeight.w500, fontSize: 25),
+                  )
+                ],
+              ),
+            ),
+          );
+          var presponse = await widget.supabase.from('projects').insert({
+            'project_name': projectName.text,
+            'date_created': formattedDate,
+            'team_name': groupName,
+            'project_description': projectDesc.text
+          }).select();
+          var uresponse = await widget.supabase.from('users').insert({
+            'user_name': userName.text,
+            'user_designation': userDesig.text,
+            'user_email': userEmail.text,
+            'project_name': projectName.text,
+            'name': userName.text,
+          }).select();
+          var bresponse = await widget.supabase.from('bugs').insert({
+            'bugs_name': bugName.text,
+            'bug_status': bugStatus.text,
+            'bugs_description': bugsDesc.text,
+            'project_name': projectName.text,
+            'date_created': formattedDate,
+          }).select();
+
+          //clear the text fields after saving
+          dispose();
+          //pop the loading screen after loading
+
+          Navigator.of(context).pop();
+          if (presponse.isNotEmpty ||
+              uresponse.isNotEmpty ||
+              bresponse.isNotEmpty) {
+            showErrorDialog(context, 'Successfully added your project 🥳',
+                'Happy Coding, Don\'t forget to squash those bugs');
+          } else {
+            showErrorDialog(context, 'Oops something went wrong 😭',
+                'Make sure you have an active internet connection');
+          }
+        }
       } catch (e) {
-        showErrorDialog(context, e.toString());
+        showErrorDialog(context, "Oops, something went wrong", e.toString());
       }
     }
 
@@ -134,22 +225,33 @@ class _AddNewScreenState extends State<AddNewScreen> {
                           TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
                     ),
                   ),
-                  const InputTextField(
-                    hintText: 'Enter User Name',
+                  InputTextField(
+                    hintText: 'Your Name',
+                    controller: name,
                     color: whiteContainer,
                     bottom: 20,
                     left: 32,
                     right: 700,
                   ),
-                  const InputTextField(
+                  InputTextField(
+                    hintText: 'Enter User Name',
+                    controller: userName,
+                    color: whiteContainer,
+                    bottom: 20,
+                    left: 32,
+                    right: 700,
+                  ),
+                  InputTextField(
                     hintText: 'Enter User Email',
                     color: whiteContainer,
+                    controller: userEmail,
                     bottom: 20,
                     left: 32,
                     right: 700,
                   ),
-                  const InputTextField(
+                  InputTextField(
                     hintText: 'Enter User Designation',
+                    controller: userDesig,
                     color: whiteContainer,
                     bottom: 20,
                     left: 32,
@@ -167,16 +269,26 @@ class _AddNewScreenState extends State<AddNewScreen> {
                   const SizedBox(
                     height: 30,
                   ),
-                  const InputTextField(
+                  InputTextField(
+                    controller: bugName,
                     hintText: 'Short Description or ID',
                     color: whiteContainer,
                     bottom: 20,
                     left: 32,
                     right: 700,
                   ),
-                  const InputTextField(
-                    hintText: 'Enter Bug Description',
+                  InputTextField(
+                    hintText: 'Bug Status',
+                    controller: bugStatus,
                     color: whiteContainer,
+                    bottom: 20,
+                    left: 32,
+                    right: 700,
+                  ),
+                  InputTextField(
+                    hintText: 'Long Description',
+                    color: whiteContainer,
+                    controller: bugsDesc,
                     bottom: 20,
                     left: 32,
                     right: 200,
